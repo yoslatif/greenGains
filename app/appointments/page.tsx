6bde644bb9fb9f511beb780a42bfbcfa683a70de
vib1240n <vibhorsgr@gmail.com>
2024-05-07 18:00:28 -0700

    Added rough homepage view and added appointments page

---
+++ app/appointments/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Appointments() {
+  return (
+    <div className="w-screen h-screen bg-black">
+      <h1 className="text-bold justify-center items-center text-white">
+        Appointments
+      </h1>
+    </div>
+  );
+}
